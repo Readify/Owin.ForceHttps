33dfd7b3e5d54f3f691ae5d79a0e0c8fff8d1f9b
Aaron Powell <me@aaron-powell.com>
2014-12-11 10:44:04 +1100

    Initial build

---
+++ Owin.ForceHttps/IAppBuilderExtensions.fs
@@ -0,0 +1,23 @@
+﻿[<System.Runtime.CompilerServices.Extension>]
+module Microsoft.Owin
+
+open Microsoft.Owin
+open System
+open System.Threading.Tasks
+
+type Owin.IAppBuilder with
+    [<System.Runtime.CompilerServices.Extension>]
+    member x.UseForcedHttps(port:int) =
+        let httpser (ctx:IOwinContext) (next:Func<Task>) =
+            match ctx.Request.Uri.Scheme with
+                | "http" ->
+                    sprintf "https://%s:%d%s" ctx.Request.Uri.Host port ctx.Request.Uri.PathAndQuery
+                        |> ctx.Response.Redirect
+                    next.Invoke()
+                | _ -> next.Invoke()
+
+        x.Use httpser |> ignore
+
+    [<System.Runtime.CompilerServices.Extension>]
+    member x.UseForcedHttps() =
+        x.UseForcedHttps(443)
