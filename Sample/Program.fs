092e55b80c1523a985751f00c5a4e38a4bdab150
Aaron Powell <me@aaron-powell.com>
2014-12-11 11:01:25 +1100

    Creating a sample application

---
+++ Sample/Program.fs
@@ -0,0 +1,17 @@
+﻿open Microsoft.Owin.Hosting
+open Sample
+
+// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    let options = StartOptions();
+    options.Urls.Add "https://localhost:44300"
+    options.Urls.Add "http://localhost:8089"
+
+    use app = WebApp.Start<Startup>(options)
+    printfn "Running on %A" options.Urls
+    System.Console.ReadLine() |> ignore
+
+    0 // return an integer exit code
---
+++ Sample/Startup.fs
@@ -0,0 +1,14 @@
+﻿namespace Sample
+
+open Owin
+open Microsoft.Owin
+open Owin.ForceHttps
+
+type Startup() =
+    member x.Configuration (app:IAppBuilder) =
+        app.UseForcedHttps(44300)
+
+        app.Run(fun ctx ->
+            ctx.Response.ContentType <- "text/plain"
+            ctx.Response.WriteAsync("Site it running")
+        ) |> ignore
